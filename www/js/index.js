f5cb253233b7dd49803f1b0c5bd6fc3d0a726616
Stanley Winata <winatastanley355@gmail.com>
2024-06-01 10:42:09 +0700

    Prettier

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
 // loadGame on first time
-document.body.onload = function(){ changeLang('indonesia');}
+document.body.onload = function () { changeLang('indonesia'); }
 
 // main menu
 var menuScreen = document.getElementById("menuScreen");
@@ -9,7 +9,7 @@ var gameScreen = document.getElementById("gameScreen");
 // New game button
 var newGameBtn = document.getElementById('newGameBtn');
 
-newGameBtn.onclick= function(){ 
+newGameBtn.onclick = function () {
   closeContNav();
   closeLangNav();
   closeProfNav();
@@ -34,20 +34,20 @@ var lvl1Btn = document.getElementById('lvl1Btn');
 var lvl2Btn = document.getElementById('lvl2Btn');
 var lvl3Btn = document.getElementById('lvl3Btn');
 
-function openContNav(){
+function openContNav() {
   contNav.style.display = 'block';
   contNav.style.width = "300px";
 }
 
-function closeContNav(){
+function closeContNav() {
   contNav.style.display = 'none';
   contNav.style.width = "0";
 }
 
-contBtn.onclick= function(){ openContNav()}; //open contNav
-closeContNavBtn.onclick= function(){ closeContNav()}; //close contNav
+contBtn.onclick = function () { openContNav() }; //open contNav
+closeContNavBtn.onclick = function () { closeContNav() }; //close contNav
 
-lvl1Btn.onclick= function(){ 
+lvl1Btn.onclick = function () {
   closeContNav();
   closeLangNav();
   closeProfNav();
@@ -64,7 +64,7 @@ lvl1Btn.onclick= function(){
   initGame();
 };
 
-lvl2Btn.onclick= function(){ 
+lvl2Btn.onclick = function () {
   closeContNav();
   closeLangNav();
   closeProfNav();
@@ -81,7 +81,7 @@ lvl2Btn.onclick= function(){
   initGame2();
 };
 
-lvl3Btn.onclick= function(){ 
+lvl3Btn.onclick = function () {
   closeContNav();
   closeLangNav();
   closeProfNav();
@@ -102,20 +102,20 @@ lvl3Btn.onclick= function(){
 
 
 // Language Button
-var langBtn = document.getElementById("langBtn");     
-var langNav = document.getElementById('langNav');  
+var langBtn = document.getElementById("langBtn");
+var langNav = document.getElementById('langNav');
 var chooseLang = document.getElementById("chooseLang");
 var closeLangNavBtn = document.getElementById("closeLangNavBtn");
-function openLangNav(){
+function openLangNav() {
   langNav.style.display = 'block';
   langNav.style.width = "300px";
 }
-function closeLangNav(){
+function closeLangNav() {
   langNav.style.display = 'none';
   langNav.style.width = "0";
 }
-langBtn.onclick= function(){ openLangNav()};
-closeLangNavBtn.onclick= function(){ closeLangNav()};
+langBtn.onclick = function () { openLangNav() };
+closeLangNavBtn.onclick = function () { closeLangNav() };
 
 // language sub section
 // sumatera lang
@@ -129,19 +129,19 @@ function closeSumNav() {
   document.getElementById("sumLangNav").style.display = 'none';
   document.getElementById("sumLangNav").style.width = "0";
 }
-sumLang.onclick= function(){ openSumNav();}
-closeSumNavBtn.onclick= function(){ closeSumNav(); }
-document.getElementById("aneuk").onclick= function(){ changeLang('aneuk')};
-document.getElementById("nias").onclick= function(){ changeLang('nias')};
-document.getElementById("minang").onclick= function(){ changeLang('minang')};
-document.getElementById("jambi").onclick= function(){ changeLang('jambi')};
-document.getElementById("kerinci").onclick= function(){ changeLang('kerinci')};
-document.getElementById("palembang").onclick= function(){ changeLang('palembang')};
-document.getElementById("besemah").onclick= function(){ changeLang('besemah')};
-document.getElementById("bangka").onclick= function(){ changeLang('bangka')};
-document.getElementById("nasal").onclick= function(){ changeLang('nasal')};
-document.getElementById("rejang").onclick= function(){ changeLang('rejang')};
-document.getElementById("lampung").onclick= function(){ changeLang('lampung')};
+sumLang.onclick = function () { openSumNav(); }
+closeSumNavBtn.onclick = function () { closeSumNav(); }
+document.getElementById("aneuk").onclick = function () { changeLang('aneuk') };
+document.getElementById("nias").onclick = function () { changeLang('nias') };
+document.getElementById("minang").onclick = function () { changeLang('minang') };
+document.getElementById("jambi").onclick = function () { changeLang('jambi') };
+document.getElementById("kerinci").onclick = function () { changeLang('kerinci') };
+document.getElementById("palembang").onclick = function () { changeLang('palembang') };
+document.getElementById("besemah").onclick = function () { changeLang('besemah') };
+document.getElementById("bangka").onclick = function () { changeLang('bangka') };
+document.getElementById("nasal").onclick = function () { changeLang('nasal') };
+document.getElementById("rejang").onclick = function () { changeLang('rejang') };
+document.getElementById("lampung").onclick = function () { changeLang('lampung') };
 
 // jawa lang
 var jawaLang = document.getElementById("jawaLang");
@@ -154,16 +154,16 @@ function closeJawaNav() {
   document.getElementById("jawaLangNav").style.display = 'none';
   document.getElementById("jawaLangNav").style.width = "0";
 }
-jawaLang.onclick= function(){ openJawaNav();}
-closeJawaNavBtn.onclick= function(){ closeJawaNav(); }
-document.getElementById("betawi").onclick= function(){ changeLang('betawi')};
-document.getElementById("sunda").onclick= function(){ changeLang('sunda')};
-document.getElementById("cirebon").onclick= function(){ changeLang('cirebon')};
-document.getElementById("jawa").onclick= function(){ changeLang('jawa')};
-document.getElementById("pemalang").onclick= function(){ changeLang('pemalang')};
-document.getElementById("osing").onclick= function(){ changeLang('osing')};
-document.getElementById("madura").onclick= function(){ changeLang('madura')};
-document.getElementById("bali").onclick= function(){ changeLang('bali')};
+jawaLang.onclick = function () { openJawaNav(); }
+closeJawaNavBtn.onclick = function () { closeJawaNav(); }
+document.getElementById("betawi").onclick = function () { changeLang('betawi') };
+document.getElementById("sunda").onclick = function () { changeLang('sunda') };
+document.getElementById("cirebon").onclick = function () { changeLang('cirebon') };
+document.getElementById("jawa").onclick = function () { changeLang('jawa') };
+document.getElementById("pemalang").onclick = function () { changeLang('pemalang') };
+document.getElementById("osing").onclick = function () { changeLang('osing') };
+document.getElementById("madura").onclick = function () { changeLang('madura') };
+document.getElementById("bali").onclick = function () { changeLang('bali') };
 
 // kalSul lang
 var kalSulLang = document.getElementById("kalSulLang");
@@ -179,16 +179,16 @@ function closeKalSulNav() {
   document.getElementById("kalSulLangNav").style.width = "0";
 }
 
-kalSulLang.onclick= function(){ openKalSulNav();}
-closeKalSulNavBtn.onclick= function(){ closeKalSulNav(); }
-document.getElementById("ngaju").onclick= function(){ changeLang('ngaju')};
-document.getElementById("kanayan").onclick= function(){ changeLang('kanayan')};
-document.getElementById("maanyan").onclick= function(){ changeLang('maanyan')};
-document.getElementById("lebang").onclick= function(){ changeLang('lebang')};
-document.getElementById("manado").onclick= function(){ changeLang('manado')};
-document.getElementById("bugis").onclick= function(){ changeLang('bugis')};
-document.getElementById("tolaki").onclick= function(){ changeLang('tolaki')};
-document.getElementById("ciacia").onclick= function(){ changeLang('ciacia')};
+kalSulLang.onclick = function () { openKalSulNav(); }
+closeKalSulNavBtn.onclick = function () { closeKalSulNav(); }
+document.getElementById("ngaju").onclick = function () { changeLang('ngaju') };
+document.getElementById("kanayan").onclick = function () { changeLang('kanayan') };
+document.getElementById("maanyan").onclick = function () { changeLang('maanyan') };
+document.getElementById("lebang").onclick = function () { changeLang('lebang') };
+document.getElementById("manado").onclick = function () { changeLang('manado') };
+document.getElementById("bugis").onclick = function () { changeLang('bugis') };
+document.getElementById("tolaki").onclick = function () { changeLang('tolaki') };
+document.getElementById("ciacia").onclick = function () { changeLang('ciacia') };
 
 // NTlang
 var NTLang = document.getElementById("NTLang");
@@ -204,21 +204,21 @@ function closeNTNav() {
   document.getElementById("NTLangNav").style.width = "0";
 }
 
-NTLang.onclick= function(){ openNTNav();}
-closeNTNavBtn.onclick= function(){ closeNTNav(); }
-document.getElementById("sasak").onclick= function(){ changeLang('sasak')};
-document.getElementById("kupang").onclick= function(){ changeLang('kupang')};
-document.getElementById("uab").onclick= function(){ changeLang('uab')};
-document.getElementById("amarasi").onclick= function(){ changeLang('amarasi')};
-document.getElementById("helong").onclick= function(){ changeLang('helong')};
-document.getElementById("fataluku").onclick= function(){ changeLang('fataluku')};
-document.getElementById("sabu").onclick= function(){ changeLang('sabu')};
-document.getElementById("lamaholot").onclick= function(){ changeLang('lamaholot')};
-document.getElementById("mangBar").onclick= function(){ changeLang('mangBar')};
-document.getElementById("mangTeng").onclick= function(){ changeLang('mangTeng')};
-document.getElementById("ende").onclick= function(){ changeLang('ende')};
-document.getElementById("adonara").onclick= function(){ changeLang('adonara')};
-document.getElementById("kedang").onclick= function(){ changeLang('kedang')};
+NTLang.onclick = function () { openNTNav(); }
+closeNTNavBtn.onclick = function () { closeNTNav(); }
+document.getElementById("sasak").onclick = function () { changeLang('sasak') };
+document.getElementById("kupang").onclick = function () { changeLang('kupang') };
+document.getElementById("uab").onclick = function () { changeLang('uab') };
+document.getElementById("amarasi").onclick = function () { changeLang('amarasi') };
+document.getElementById("helong").onclick = function () { changeLang('helong') };
+document.getElementById("fataluku").onclick = function () { changeLang('fataluku') };
+document.getElementById("sabu").onclick = function () { changeLang('sabu') };
+document.getElementById("lamaholot").onclick = function () { changeLang('lamaholot') };
+document.getElementById("mangBar").onclick = function () { changeLang('mangBar') };
+document.getElementById("mangTeng").onclick = function () { changeLang('mangTeng') };
+document.getElementById("ende").onclick = function () { changeLang('ende') };
+document.getElementById("adonara").onclick = function () { changeLang('adonara') };
+document.getElementById("kedang").onclick = function () { changeLang('kedang') };
 
 // ambon & papualang
 var amPaLang = document.getElementById("amPaLang");
@@ -234,10 +234,10 @@ function closeAmPaNav() {
   document.getElementById("amPaLangNav").style.width = "0";
 }
 
-amPaLang.onclick= function(){ openAmPaNav();}
-closeAmPaNavBtn.onclick= function(){ closeAmPaNav(); }
-document.getElementById("papua").onclick= function(){ changeLang('papua')};
-document.getElementById("biak").onclick= function(){ changeLang('biak')};
+amPaLang.onclick = function () { openAmPaNav(); }
+closeAmPaNavBtn.onclick = function () { closeAmPaNav(); }
+document.getElementById("papua").onclick = function () { changeLang('papua') };
+document.getElementById("biak").onclick = function () { changeLang('biak') };
 
 // asing lang
 var asingLang = document.getElementById("asingLang");
@@ -253,36 +253,36 @@ function closeAsingNav() {
   document.getElementById("asingLangNav").style.width = "0";
 }
 
-asingLang.onclick= function(){ openAsingNav();}
-closeAsingNavBtn.onclick= function(){ closeAsingNav(); }
-document.getElementById("indonesia").onclick= function(){ changeLang('indonesia')};
-document.getElementById("english").onclick= function(){ changeLang('english')};
-document.getElementById("chinese").onclick= function(){ changeLang('chinese')};
-document.getElementById("italian").onclick= function(){ changeLang('italian')};
-document.getElementById("japan").onclick= function(){ changeLang('japan')};
+asingLang.onclick = function () { openAsingNav(); }
+closeAsingNavBtn.onclick = function () { closeAsingNav(); }
+document.getElementById("indonesia").onclick = function () { changeLang('indonesia') };
+document.getElementById("english").onclick = function () { changeLang('english') };
+document.getElementById("chinese").onclick = function () { changeLang('chinese') };
+document.getElementById("italian").onclick = function () { changeLang('italian') };
+document.getElementById("japan").onclick = function () { changeLang('japan') };
 //..................................................................
 
 
 // Profile Button
 var profBtn = document.getElementById("profBtn");
 var closeProfNavBtn = document.getElementById("closeProfNavBtn");
-function openProfNav(){
+function openProfNav() {
   profNav.style.display = 'block';
   profNav.style.width = "300px";
 }
-function closeProfNav(){
+function closeProfNav() {
   profNav.style.display = 'none';
   profNav.style.width = "0";
 }
-profBtn.onclick= function(){ openProfNav()};
-closeProfNavBtn.onclick= function(){ closeProfNav()};
+profBtn.onclick = function () { openProfNav() };
+closeProfNavBtn.onclick = function () { closeProfNav() };
 
 var chooseGender = document.getElementById("chooseGender");
 var maleBtn = document.getElementById("male");
 var femaleBtn = document.getElementById("female");
 
-maleBtn.onclick = function() {changeGender('Male'); closeProfNav();}
-femaleBtn.onclick = function() {changeGender('Female'); closeProfNav();}
+maleBtn.onclick = function () { changeGender('Male'); closeProfNav(); }
+femaleBtn.onclick = function () { changeGender('Female'); closeProfNav(); }
 
 function changeGender(gender) {
   if (gender === 'Male') {
@@ -299,16 +299,16 @@ function changeGender(gender) {
 // Credit Button
 var creditBtn = document.getElementById("creditBtn");
 var closeCreditNavBtn = document.getElementById("closeCreditNavBtn");
-function openCreditNav(){
+function openCreditNav() {
   creditNav.style.display = 'block';
   creditNav.style.width = "80%";
 }
-function closeCreditNav(){
+function closeCreditNav() {
   creditNav.style.display = 'none';
   creditNav.style.width = "0";
 }
-creditBtn.onclick = function() {openCreditNav()}
-closeCreditNavBtn.onclick = function() {closeCreditNav()}
+creditBtn.onclick = function () { openCreditNav() }
+closeCreditNavBtn.onclick = function () { closeCreditNav() }
 //..................................................................
 
 // sideNav
@@ -318,20 +318,20 @@ var sideNavNewGame = document.getElementById('sideNavNewGame'); //sideNav new ga
 var sideNavLang = document.getElementById('sideNavLang'); //sideNav lang which opens langBtn
 var sideNavBack = document.getElementById('sideNavBack'); //sideNav Back to main Menu btn
 function openSideNav() {
-    pause = true;
-    document.getElementById('sideNav').style.display = 'block';
-    document.getElementById('sideNav').style.width = "300px";
+  pause = true;
+  document.getElementById('sideNav').style.display = 'block';
+  document.getElementById('sideNav').style.width = "300px";
 }
 function closeSideNav() {
-    pause = false;
-    document.getElementById('sideNav').style.display = 'none';
-    document.getElementById('sideNav').style.width = "0px";
+  pause = false;
+  document.getElementById('sideNav').style.display = 'none';
+  document.getElementById('sideNav').style.width = "0px";
 }
-sideNavBtn.onclick = function() {openSideNav();}
-closeSideNavBtn.onclick = function() {closeSideNav();}
-sideNavNewGame.onclick = function() {location.reload();}
+sideNavBtn.onclick = function () { openSideNav(); }
+closeSideNavBtn.onclick = function () { closeSideNav(); }
+sideNavNewGame.onclick = function () { location.reload(); }
 // sideNavLang.onclick = function() { openLangNav();}
-sideNavBack.onclick= function(){ 
+sideNavBack.onclick = function () {
   closeContNav();
   closeLangNav();
   closeProfNav();
